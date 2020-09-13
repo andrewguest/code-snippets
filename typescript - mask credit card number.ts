fc8fb9c05366b60c78f436717543842cb4b9619c
Andrew <andrew@aguest.me>
2020-09-12 20:50:28 -0500

    Update and rename typescript - mask credit card number.js to typescript - mask credit card number.ts

--- typescript - mask credit card number.ts
+++ typescript - mask credit card number.ts
@@ -4,7 +4,7 @@ This will take an input (a credit card number) and, if it's over 4 digits long, 
 So, 1234567890123456 becomes ############3456
 */
 
-function maskCreditCardNumber(cc: string): string {
+maskCreditCardNumber(cc: string): string {
   let masked_cc = cc.replace(/\d(?=\d{4})/g, "#");
   console.log(masked_cc);
   return masked_cc
